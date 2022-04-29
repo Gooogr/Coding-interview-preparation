b95687bcd5feef1541079543874b6f3cc99a8883
Grigoriy <gusarovga@ya.ru>
2022-04-29 10:50:56 +0300

    Update 20_valid_parentheses.py
    Add comments

--- Stack/20_valid_parentheses.py
+++ Stack/20_valid_parentheses.py
@@ -5,13 +5,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        # Initialize list for stack constructure
         stack = []
+        # Map parenthood between brackets
         map_symbols = {"]":"[", "}":"{", ")":"("}
+        
         for item in s:
+            # If we got opening bracket - add it to the stack
             if item in map_symbols.values():
                 stack.append(item)
+                
+            # Else - we got closing bracket
             else:
+                # Check if stack is already empty or previous bracket doesn't match with current one
                 if stack == [] or stack[-1] != map_symbols[item]:
                     return False
+                # If everything is OK - remove opening bracket from stack, because we have found a pair for it.
                 stack.pop()
+         
+        # Return final stack state after all iterations 
         return stack == []
