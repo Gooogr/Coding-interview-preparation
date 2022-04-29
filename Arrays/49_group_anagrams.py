c2bfa202ee8614da85010efb733dd80e95087fc8
Gooogr <gusarovga@ya.ru>
2022-04-29 12:12:23 +0300

    update 49 task

--- Arrays/49_group_anagrams.py
+++ Arrays/49_group_anagrams.py
@@ -1,24 +1,29 @@
 
 #https://leetcode.com/problems/group-anagrams/ 
-# My initial approach. Far from optimal :)
 class Solution(object):
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        ## Need sorting to keep the same keys order for every anagram 
+        # def get_anagram_state(s):
+        #     anagram_dict = {}
+        #     for item in s:
+        #         if item not in anagram_dict:
+        #             anagram_dict[item] = s.count(item)
+        #     ordered_dict = {k: v for k, v in sorted(list(anagram_dict.items()))}
+        #     return str(ordered_dict)
         
-        def get_anagram_dict(s):
-            anagram_dict = {}
-            for item in s:
-                if item not in anagram_dict:
-                    anagram_dict[item] = s.count(item)
-            ordered_dict = {k: v for k, v in sorted(list(anagram_dict.items()))}
-            return ordered_dict
+        def get_anagram_state(s):
+            counter = [0] * 26 # a...z
+            for letter in s:
+                counter[ord(letter) - ord('a')] += 1
+            return str(counter)
         
         result_hash = dict()
         for word in strs:
-            anagram_dict = str(get_anagram_dict(word))
+            anagram_dict = str(get_anagram_state(word))
             if anagram_dict not in result_hash:
                 result_hash[anagram_dict] = [word]
             else:
