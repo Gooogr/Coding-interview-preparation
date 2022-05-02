80043928a66eb3d0b0eec75b2b9abbedd7f5329b
Gooogr <gusarovga@ya.ru>
2022-05-02 23:42:08 +0300

    add task 155 optimal

--- Stack/155_min_stack.py
+++ Stack/155_min_stack.py
@@ -1,6 +1,5 @@
 # https://leetcode.com/problems/min-stack/
 # Naive approach without getMin optimization
-
 class MinStack(object):
 
     def __init__(self):
@@ -34,3 +33,51 @@ class MinStack(object):
         :rtype: int
         """
         return min(self.stack)
+
+# Faster approach with min_stack tracking
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        # Select minimal value for the current stack state
+        min_val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(min_val)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.min_stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
