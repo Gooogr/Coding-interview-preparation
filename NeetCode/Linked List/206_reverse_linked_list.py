321026fff94a8b29277c44a3aea22b1a65edfd48
Gooogr <gusarovga@ya.ru>
2022-11-14 21:00:14 +0300

    fix: minor code update

--- NeetCode/Linked List/206_reverse_linked_list.py
+++ NeetCode/Linked List/206_reverse_linked_list.py
@@ -1,10 +1,10 @@
 # https://leetcode.com/problems/merge-two-sorted-lists/
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 # Iterative approach
 class Solution:
