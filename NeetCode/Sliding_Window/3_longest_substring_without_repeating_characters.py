32c31c868552fbcdc5e0b76be7acf3d0ba676bac
Gooogr <gusarovga@ya.ru>
2022-11-22 22:00:36 +0300

    feat: add task 3

--- NeetCode/Sliding_Window/3_longest_substring_without_repeating_characters.py
+++ NeetCode/Sliding_Window/3_longest_substring_without_repeating_characters.py
@@ -1,21 +1,16 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        subset = set()
-        result = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         l = 0
-        
+        max_len = 0
+        string_char = set()
         for r in range(len(s)):
-            # If truncate set from the left, until we eliminate all duplicates in substring
-            while s[r] in subset:
-                subset.remove(s[l])
+            # remove repeated letters from hash from the left side of substring
+            # make step with left pointer
+            while s[r] in string_char:
+                string_char.remove(s[l])
                 l += 1
-            # Now we can safely add new element
-            subset.add(s[r])
-            # Check elemnts amount in our substing. The same amount if hash 
-            result = max(result, len(subset))
-        return result
+            # add new unique letter to the hash from right side of substring
+            string_char.add(s[r])
+            max_len = max(max_len, r - l  + 1)
+        return max_len
