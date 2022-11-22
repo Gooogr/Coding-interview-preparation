f576ba9754d8d4671e502f6089791037ebf73fd8
Gooogr <gusarovga@ya.ru>
2022-11-22 20:06:58 +0300

    feat: add task 15

--- NeetCode/Two_Pointers/15_3sum.py
+++ NeetCode/Two_Pointers/15_3sum.py
@@ -1,28 +1,28 @@
 # https://leetcode.com/problems/3sum/
 
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        res = []
-        nums.sort()
+from typing import List
 
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        result = []
+        for idx, val1 in enumerate(nums):
+            # handle repeated triplets
+            if idx > 0 and val1 == nums[idx - 1]:
                 continue
-
-            l, r = i + 1, len(nums) - 1
+            # solve 2sum problem for sorted list
+            l = idx + 1
+            r = len(nums) - 1
             while l < r:
-                threeSum = nums[i] + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
+                summ = nums[l] + nums[r] + val1
+                if summ == 0:
+                    result.append([val1, nums[l], nums[r]])
+                    # push pointer further and skip duplets
+                    l +=1
+                    while nums[l] == nums[l-1] and l < r:
                         l += 1
-        return res
+                elif summ < 0:
+                    l +=1
+                else:
+                    r -=1
+        return result
