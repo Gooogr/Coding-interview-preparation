b4eb3a752287505f5343495ccafe1c58b512d1f8
Gooogr <gusarovga@ya.ru>
2022-12-02 15:32:49 +0300

    fix: update task 21

--- NeetCode/Linked_List/21_merge_two_sorted_lists.py
+++ NeetCode/Linked_List/21_merge_two_sorted_lists.py
@@ -6,20 +6,27 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
+        
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode() 
-        tail = dummy
+        result = ListNode(0, None) # we will skip 0 at return stage
+        tail = result              # because we need to store head for return
+        
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1    # connect object to linked list
-                list1 = list1.next   # make step
+                tail.next = list1
+                list1 = list1.next
             else:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
-        # if one linked list is bigger than other - add final chunk
-        tail.next = list1 or list2
-        return dummy.next
+            
+        # Add leftover from list    
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+        # return head without dummy 0 value    
+        return result.next
+        
         
