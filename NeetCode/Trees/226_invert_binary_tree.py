d8f1c83810e4fc4828eaba923656e3bbd7f77838
Gooogr <gusarovga@ya.ru>
2023-01-25 17:12:01 +0300

    feat: add another solution

--- NeetCode/Trees/226_invert_binary_tree.py
+++ NeetCode/Trees/226_invert_binary_tree.py
@@ -7,10 +7,24 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+# Reversed changes approach.
+# Create stack and then switch from leafs to root
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
+# Forward changes approach
+# Switch from root children to leafs
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.right) 
+        self.invertTree(root.left)
         return root
         
