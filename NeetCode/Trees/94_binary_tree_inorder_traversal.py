3f0b1d71638ddd4a09cfcc2740ea1973eb54fe69
Gooogr <gusarovga@ya.ru>
2023-01-25 23:26:30 +0300

    fix: minor code update

--- NeetCode/Trees/94_binary_tree_inorder_traversal.py
+++ NeetCode/Trees/94_binary_tree_inorder_traversal.py
@@ -22,12 +22,10 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result = []
         def traverse(node: Optional[TreeNode]):
-            if not node:
-                return 
-            traverse(node.left)
-            result.append(node.val)
-            traverse(node.right)
+            if node:
+                traverse(node.left)
+                result.append(node.val)
+                traverse(node.right)
         traverse(root)
         return result
 
-# iterative approach
