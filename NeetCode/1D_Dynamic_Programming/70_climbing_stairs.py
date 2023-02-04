460bf1219646f7cbbe5d81efdbefa4e46ce89712
Gooogr <gusarovga@ya.ru>
2023-02-04 18:21:27 +0300

    fix: add more sensible solution

--- NeetCode/1D_Dynamic_Programming/70_climbing_stairs.py
+++ NeetCode/1D_Dynamic_Programming/70_climbing_stairs.py
@@ -1,15 +1,15 @@
 # https://leetcode.com/problems/climbing-stairs/
 
-# Bottom up dynamic approach
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # edge cases
-        if n <= 3:
-            return n
-        n1, n2 = 2, 3
 
-        for i in range(4, n + 1):
-            temp = n1 + n2
-            n1 = n2
-            n2 = temp
-        return n2
+        def get_climbs(n: int, cache: dict={}):
+            if n < 2:      # Base case
+                return 1
+            if n in cache: # Memorization 
+                return cache[n]
+            # ladder[i] = ladder[i - 1] + ladder[i - 2] # by final possible steps amount
+            cache[n] = get_climbs(n - 1, cache) + get_climbs(n - 2, cache)
+            return cache[n]
+        result = get_climbs(n)
+        return result
