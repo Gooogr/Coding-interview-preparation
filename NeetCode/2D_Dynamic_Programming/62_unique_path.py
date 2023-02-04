2ca1e2401a48f0b57cf8bfe39338870250d65b63
Gooogr <gusarovga@ya.ru>
2023-02-04 18:29:14 +0300

    fix: minor comment update

--- NeetCode/2D_Dynamic_Programming/62_unique_path.py
+++ NeetCode/2D_Dynamic_Programming/62_unique_path.py
@@ -11,7 +11,7 @@
 # F(2, 3) = F(1, 3) + F(2, 2)
 # ...
 
-# Time O(m*n)
+# Time O(m*n) - keep only necessary combination
 # Memory O(m + n) - from trimmed recursion tree
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
@@ -20,7 +20,7 @@ class Solution:
                 return 1
             if m == 0 or n == 0:
                 return 0
-            key = str(f'{m} {n}')
+            key = str(f'{m} {n}') #possible optimization (m, n) and (n, m) is same values
            
             if key in cache:
                 return cache[key]
