293620f3adcd8767e0b59101f67d96af0cf7db64
Gooogr <gusarovga@ya.ru>
2023-02-17 19:25:47 +0300

    feat: add tabular solutions

--- NeetCode/2D_Dynamic_Programming/62_unique_path.py
+++ NeetCode/2D_Dynamic_Programming/62_unique_path.py
@@ -1,5 +1,4 @@
 # https://leetcode.com/problems/unique-paths/
-# https://youtu.be/oBt53YbR9Kk?t=3552 explanation from FreeCodeCamp
 
 # Base cases
 # F(i, 0) = 0, where i is any number
@@ -28,3 +27,34 @@ class Solution:
             return cache[key]
         result = get_path(m, n)
         return result
+
+
+# Tabular approach
+# Time O(m*n)  - two for loops
+# Memory O(m*n) - we store full dp matrix
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # We initialize our dp by 1 because 
+        # F(i, 1) = 1, F(1, j) = 1
+        # We can move only in one direction in this "corridors"
+        dp = [[1] * m for _ in range(n)] # because 
+        print(dp)
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
+
+
+# Memory optimized tabular approach
+# We need only two last lines
+# Time O(m*n) 
+# Memory O(n) 
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        for _ in range(m - 1):
+            newRow = [1] * n                       # add new 1D "corridor"
+            for j in range(1, n):
+                newRow[j] = newRow[j - 1] + row[j] # expand all path with this 1D 
+            row = newRow
+        return row[-1]
