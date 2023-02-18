dc1c0a485e93ebc0606379ed768bdedba979a3ce
Gooogr <gusarovga@ya.ru>
2023-02-18 17:16:12 +0300

    feat: add task 424

---
+++ NeetCode/Sliding_Window/424_longest_repeating_character_replacement.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/longest-repeating-character-replacement
+
+
+# My solution. 
+# O(n^2) in time complexity because of max(arr)
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        arr = [0] * 26
+        max_length = 0
+        for r in range(len(s)):
+            arr_idx = ord(s[r]) - ord('A')
+            arr[arr_idx] += 1
+            
+            # increase max_length if we meet cindition
+            if (r - l + 1) - max(arr) <= k:
+                max_length = max(max_length, (r - l + 1))
+            # truncate substring from the left, until we meet condition again
+            else:
+                while (r - l + 1) - max(arr) > k and l < r:
+                    arr_idx = ord(s[l]) - ord('A')
+                    arr[arr_idx] -= 1
+                    l += 1
+        return max_length
+    
+# O(n) in time complexity because of caching
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freq_map = {}
+        l = 0
+        max_freq = 0
+        max_substr_len = 0
+        for r in range(len(s)):
+            # update max based on previously added char
+            freq_map[s[r]] = freq_map.get(s[r], 0) + 1
+            max_freq = max(freq_map[s[r]], max_freq)
+
+            # penalty for l+=1 if we failed condition
+            # thus (r - l + 1) can't be more than max_freq for next iteration
+            if (r - l + 1) - max_freq > k:
+                freq_map[s[l]] -= 1
+                l += 1
+
+            max_substr_len = max(max_substr_len, (r - l + 1))
+        return max_substr_len
