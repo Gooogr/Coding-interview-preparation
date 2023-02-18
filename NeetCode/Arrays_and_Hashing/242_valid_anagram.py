b5e5040bc25affa8b5c9bf32f6820a48a47e2204
Gooogr <gusarovga@ya.ru>
2023-02-19 01:13:46 +0300

    feat: add list-based solution

--- NeetCode/Arrays_and_Hashing/242_valid_anagram.py
+++ NeetCode/Arrays_and_Hashing/242_valid_anagram.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/valid-anagram/
+
 class Solution:
     # Complexity O(N), Memory O(N)
     # Can be solved with memory O(1) but complexity O(n*logn) by
@@ -14,3 +15,18 @@ class Solution:
         for item in t:
             t_count[item] += 1
         return s_count == t_count
+    
+# Or more memory efficient, because 
+# list need less space than dict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        def get_str_code(s: str):
+            arr = [0] * 26
+            for char in s:
+                idx = ord(char) - ord('a')
+                arr[idx] += 1
+            return arr
+
+        return get_str_code(s) == get_str_code(t)
