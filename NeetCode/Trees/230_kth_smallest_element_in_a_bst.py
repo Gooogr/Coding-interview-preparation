83554050c22efd992ccc0dcd611a74ecc8cacc60
Gooogr <gusarovga@ya.ru>
2023-02-27 21:50:28 +0300

    feat: add task 230

---
+++ NeetCode/Trees/230_kth_smallest_element_in_a_bst.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst
+
+from typing import Optional
+import heapq
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Naive approach with heap - iterate over all tree (don't use it search features),
+# add values to min heap and pop k-th element
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        arr = []
+
+        def dfs(root):
+            if not root:
+                return None
+            arr.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        heapq.heapify(arr)
+        while k > 0:
+             result = heapq.heappop(arr)
+             k -= 1
+        return result
