6f5ad1344851fadbd157191d9652e2d066543ce1
Gooogr <gusarovga@ya.ru>
2023-03-01 22:10:19 +0300

    feat: update task 238

--- NeetCode/Arrays_and_Hashing/238_product_of_array_except_self.py
+++ NeetCode/Arrays_and_Hashing/238_product_of_array_except_self.py
@@ -1,23 +1,43 @@
 # https://leetcode.com/problems/product-of-array-except-self/
-# Complexity O(n), Memory O(1)
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        result = [1]*len(nums)
-        p = 1
+
+from typing import List
+
+# Naive solution with prefix and postfix lists
+# Time complexity O(n)
+# Space complexity O(n)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # collect prefix and postfix values
+        prefix = [1]
+        postfix = [1]
+        for num in nums:
+            prefix.append(prefix[-1] * num)
+        for num in nums[::-1]:
+            postfix.append(postfix[-1] * num)
+        # get final result - multiply values from edges
+        res = []
+        for idx in range(len(prefix) - 1):
+            res.append(prefix[idx] * postfix[len(postfix) - 2 - idx])
+        return res
+
+
+# Inplace approach
+# Time complexity O(n) 
+# Space complexity O(1)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
         
-        # Find products to the left from every element
+        # left traverse
+        p = 1
         for idx in range(len(nums)):
-            result[idx] = p
+            res[idx] = p
             p *= nums[idx]
-            
-        # Find products to the right from every element. 
-        # Multiply to the sub-results from the previous iteration
+
+        # right traverse
         p = 1
         for idx in range(len(nums) - 1, -1, -1):
-            result[idx] *= p
-            p *=  nums[idx]
-        return result
+            res[idx] *= p
+            p *= nums[idx]
+
+        return res
