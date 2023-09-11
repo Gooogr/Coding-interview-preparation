e4f7f6f2f5ae94e7bbeea2ba1d75156a96954699
grigoriy <gusarov.sys@gmail.com>
2023-09-11 12:52:17 +0300

    fix: update task 21

--- Grind75/21_merge_two_sorted_lists.py
+++ Grind75/21_merge_two_sorted_lists.py
@@ -9,21 +9,19 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode()
-        curr = head
-        while list1 is not None and list2 is not None:
-            val1 = list1.val
-            val2 = list2.val
-            if val1 <= val2:
-                curr.next = list1
+        head = ListNode(0)
+        tail = head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
                 list1 = list1.next
             else:
-                curr.next = list2
+                tail.next = list2
                 list2 = list2.next
-            curr = curr.next
+            tail = tail.next
         
-        if list1 is not None:
-            curr.next = list1
+        if list1:
+            tail.next = list1
         else:
-            curr.next = list2
+            tail.next = list2
         return head.next
